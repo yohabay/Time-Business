45e320043960c4da1461b4c13657e976e276a2a5
yohannis <Abayyohannis2014@gmail.com>
2024-11-22 05:21:01 +0300

    add the image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import heroimage from "../../assets/website/image.png";
+import heroimage from "../../assets/website/hero1.jpg";
 
 const Hero = () => {
   // State to toggle the visibility of additional content
@@ -11,16 +11,30 @@ const Hero = () => {
   };
 
   return (
-    <div className="dark:bg-gray-950 dark:text-white duration-300 ">
+    <div className="dark:bg-gray-950 dark:text-white duration-300">
       <div className="container min-h-[620px] flex mt-10 sm:mt-0">
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 place-items-center">
           {/* Image section */}
           <div data-aos="zoom-in" className="order-1 sm:order-2 relative">
+            {/* Main Hero Image */}
             <img
               src={heroimage}
-              alt="images"
-              className="w-full sm:max-w-[280px] md:max-w-[430px]"
+              alt="hero"
+              className="w-full h-[280px] sm:max-w-[380px] md:max-w-[430px] shadow-lg shadow-black/30"
             />
+            {/* Overlay Image with text next to it */}
+            {/* <div className="absolute top-5 left-5 flex items-start space-x-3">
+              <img
+                src={overlayImage}
+                alt="overlay"
+                className="w-[120px] h-[180px]"
+              />
+              <div className="text-primary flex flex-col justify-center ">
+                <h3 className="text-2xl font-bold" style={{ fontSize: "30px" }}>
+                  Our Digital Health App
+                </h3>
+              </div>
+            </div> */}
             <div
               data-aos="slide-right"
               className="absolute -bottom-5 -right-8 px-4 py-2 rounded-xl bg-white dark:bg-gray-900 dark:text-white shadow-md"
@@ -28,7 +42,7 @@ const Hero = () => {
           </div>
 
           {/* Text section */}
-          <div className="space-y-5 order-2 sm:order-1 xl:pr-40 ">
+          <div className="space-y-5 order-2 sm:order-1 xl:pr-40">
             <h1
               data-aos="fade-up"
               className="text-4xl sm:text-5xl font-semibold"
