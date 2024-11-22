01cf7feafba0c0f74a6e77f8b995c3b1747efe1d
yohannis <Abayyohannis2014@gmail.com>
2024-11-22 05:33:55 +0300

    next step

--- src/components/BrandsLogo/BrandsLogo.jsx
+++ src/components/BrandsLogo/BrandsLogo.jsx
@@ -7,33 +7,12 @@ const BrandsLogo = () => {
       <h1 className="text-center text-3xl font-semibold mb-6">
         Powering Next-Gen Companies
       </h1>
-      <div className="py-6 md:px-32 flex flex-wrap items-center justify-center gap-6 sm:gap-3 md:gap-8">
+      <div className="py-6 md:px-32 flex flex-wrap items-center justify-start gap-6 sm:gap-3 md:gap-8">
         {/* Brands Logos */}
         <div className="flex justify-center items-center w-28 sm:w-24">
           <img
             src={Brand1}
             alt="Brand 1"
-            className="w-full h-auto object-contain transition-transform transform hover:scale-110"
-          />
-        </div>
-        <div className="flex justify-center items-center w-24 sm:w-20">
-          <img
-            src={Brand2}
-            alt="Brand 2"
-            className="w-full h-auto object-contain transition-transform transform hover:scale-110"
-          />
-        </div>
-        <div className="flex justify-center items-center w-24 sm:w-20">
-          <img
-            src={Brand1}
-            alt="Brand 1"
-            className="w-full h-auto object-contain transition-transform transform hover:scale-110"
-          />
-        </div>
-        <div className="flex justify-center items-center w-24 sm:w-20">
-          <img
-            src={Brand2}
-            alt="Brand 2"
             className="w-full h-auto object-contain transition-transform transform hover:scale-110"
           />
         </div>
